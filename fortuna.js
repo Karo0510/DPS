26cf51daa37dcb76760fc72dec4c41a27d3b79a8
Karo0510 <karma0510@gmail.com>
2021-11-09 21:30:34 +0100

    span

--- fortuna.js
+++ fortuna.js
@@ -3,19 +3,21 @@ var game = {
   zdobyte : 0,
   zycia : 1,
 }
-alert(data[0]['country']);
+//alert(data[0]['country']);
 var elem = document.getElementById("panstwa");
-elem.innerHTML =data[0]['country'];
+//elem.innerHTML =data[0]['country'];
 
 //alert(data.length);
 //alert(data[0]['country'][2]);
 
- /*for (var i = 0; i < data[0]['country'].length; i += 1) {
-    alert(data[0]['country'][i]);  
-  }*/
+//alert(data[0]['country'].length);
+
+for (var i = data[0]['country'].length-1; i >= 0; i -= 1) 
+{
+    addElement("wrap", data[0]['country'][i]);
+}
 
 
-addElement("wrap");
 //LISTENERS
 
 document.getElementById("graj").addEventListener("click", Sprawdz_Litery); 
@@ -33,29 +35,60 @@ function wyswietlInfo()
 }
 
 
-//FUNKCJE
-function Sprawdz_Litery(){
-  var liter = document.getElementById("wpisz_litere").value;
-  alert(liter);
-  alert(getRandomInt(10,20));
+
+function Ent(e)
+{ 
+    console.log(e)
+    if (e.key == "ArrowLeft") 
+    {
+      Sprawdz_Litery();
+    }
 }
 
+document.getElementById("wpisz_litere").addEventListener("keydown", Ent, false);
+console.log(document.getElementsByClassName("mystyle"));
 
-function addElement(mydiv)
+
+//FUNKCJE
+
+
+function Sprawdz_Litery()
 {
- 
+  var liter = document.getElementById("wpisz_litere").value;
+
+  for (var i = 0; i < data[0]['country'].length; i+= 1)
+  {
+    liter_h = data[0]['country'][i];
+
+    if (liter == liter_h.toUpperCase())
+    {
+        document.getElementsByClassName("mystyle")[i].style.backgroundColor="white";
+    }
+    //alert("blad");
+  }
+  //alert(liter);
+  //alert(getRandomInt(10,20));
+}
+
+function insertAfter(Node, newNode) 
+{
+  Node.parentNode.insertBefore(newNode, Node.nextSibling);
+}
+
+function addElement(mydiv, letter)
+{ 
   newDiv = document.createElement("span");
-  newDiv.innerHTML = "jasiokotek";
+  newDiv.innerHTML = letter;
 
   my_div = document.getElementById(mydiv);
-  document.body.insertBefore(newDiv, my_div);
-
-  newDiv2 = document.createElement("span");
-  newDiv2.innerHTML = "jasiokotek2";
-  document.body.insertBefore(newDiv2, my_div.nextSibling);
+  //document.body.insertBefore(newDiv, my_div);
+  insertAfter(my_div, newDiv);
 
   newDiv.classList.add("mystyle");  
+  console.log(newDiv)
 }
+
+
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
